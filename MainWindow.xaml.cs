b847906c4541970517adc58081109e2ea152d904
ike-123 <98366233+ike-123@users.noreply.github.com>
2026-04-11 14:40:44 +0100

    Add Gemini API. Generate Paraphrased text when button is clicked.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Google.GenAI;
+using Google.GenAI.Types;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +21,57 @@ namespace AiRewordApp
 		public MainWindow()
 		{
 			InitializeComponent();
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
+		}
+
+		private async void Button_Click(object sender, RoutedEventArgs e)
+		{
+			//string sentence = "The quick brown fox jumps over the lazy dog.";
+
+			//ensure textbox is not empty before button click
+			if (InputTextBox.Text != "")
+			{
+
+
+
+				string sentence = InputTextBox.Text;
+
+				//reset textbox after button click
+				InputTextBox.Text = "";
+
+				//Disable button after click to prevent multiple clicks
+				GenerateButton.IsEnabled = false;
+
+				string ParaphrasedText = await GenerateGeminiContent(sentence);
+
+
+				GenerateButton.IsEnabled = true;
+
+
+				ParaphrasedTextBox.Text = ParaphrasedText;
+
+			}
+
+
+		}
+
+
+		public async Task<string> GenerateGeminiContent(string text)
+		{
+			//Client gets API key from environment variable
+
+			var client = new Client();
+
+			var response = await client.Models.GenerateContentAsync(model: "gemini-2.5-flash", contents: $"Paraphrase the following sentence: {text}. Only paraphrase once");
+
+
+
+			return response.Text;
+		
 		}
 	}
 }
